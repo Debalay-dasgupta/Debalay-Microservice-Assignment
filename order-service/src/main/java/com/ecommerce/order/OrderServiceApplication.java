686604529937f23851a48c8f09979b60d2825899
Debalay-dasgupta <debalaydasgupta@gmail.com>
2026-02-20 11:25:41 +0530

    RestCLient

--- order-service/src/main/java/com/ecommerce/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/ecommerce/order/OrderServiceApplication.java
@@ -3,16 +3,17 @@ package com.ecommerce.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.client.RestClient;
+
 
 @SpringBootApplication
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
     }
-    
+
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
     }
 }
